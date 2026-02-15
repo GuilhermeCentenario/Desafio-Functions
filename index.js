29b8e070ee2a6edaae2b612b0fe44a11932e64f1
Guilherme <157997399+GuilhermeCentenario@users.noreply.github.com>
2026-02-15 20:18:14 -0300

    Finalização

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let results = subRankeds(10, 11)
+let results = subRankeds(101, 1)
+let frase = ("O Herói tem de saldo de ")
 function subRankeds (wins,defeats){
     let win = wins - defeats
     return win
@@ -6,7 +7,17 @@ function subRankeds (wins,defeats){
 
 
 if (results < 10){
-    console.log("O Herói tem de saldo de " + results + " Vitórias" + " e está no nível de Ferro")
-}else if(results){
-    
+    console.log(frase + results + " Vitórias" + " e está no nível de Ferro")
+}else if(results>10 && results <= 20){
+    console.log(frase + results + " Vitórias" + " e está no nível de Bronze")
+}else if(results>20 && results <= 50){
+    console.log(frase + results + " Vitórias" + " e está no nível de Prata")
+}else if(results>50 && results <= 80){
+    console.log(frase + results + " Vitórias" + " e está no nível de Ouro")
+}else if(results>80 && results <= 90){
+    console.log(frase + results + " Vitórias" + " e está no nível de Diamante")
+}else if(results>90 && results <= 100){
+    console.log(frase + results + " Vitórias" + " e está no nível de Lendário")
+}else if(results >= 101){
+    console.log(frase + results + " Vitórias" + " e está no nível de Imortal")
 }
